42def37f2e920d6dee1eed26e8ebeea4951296b1
raymondyiga123 <91730337+raymondyiga123@users.noreply.github.com>
2021-11-05 16:33:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,45 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("amount").value;
     
+    if(principal == ""){
+        alert("Please enter the amount(Principal), Field cannot be empty");
+        document.getElementById("amount").value="";
+        amount.focus();
+        return;
+  } 
+  if(principal == 0){
+        alert("The Principal cannot be 0, Please enter a valid amount above 0");
+        document.getElementById("amount").value="";
+        amount.focus();
+        return;
+  }
+  if(principal <= 0){
+    alert("Please Enter a Positive Number/Amount");
+    document.getElementById("amount").value="";
+    amount.focus();
+    return;
+  }
+
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var simple_interest = (principal * years * rate) / 100;
+
+    var NowDate = new Date();
+    var CurrentYear = parseInt(NowDate.getFullYear()) + parseInt(years);
+    
+    var display_result = document.getElementById("result");
+    display_result.innerHTML = "If you deposit " + "<span class='highlight'>" + principal + "</span>,"  + "<br> at an interest rate of "+ "<span class='highlight'>" + rate + "</span>%." + "<br> You will receive an amount of " + "<span class='highlight'>" + simple_interest + "</span>" + ", <br> in the year " + "<span class='highlight'>" + CurrentYear + "</span>";
 }
-        
+
+function SliderValue()
+{
+    var slider = document.getElementById("rate");
+    var output = document.getElementById("rate_display");
+    output.innerHTML = slider.value; 
+
+    slider.oninput = function() 
+    {
+        output.innerHTML = this.value;
+    }  
+}
